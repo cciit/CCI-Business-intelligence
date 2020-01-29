92364ab37b61600dc41246c381d0deccfae42d07
cciit <60438931+cciit@users.noreply.github.com>
2020-01-29 10:49:10 -0500

    Create head.php

---
+++ head.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>CCI BI</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css"> <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"> <!-- Ionicons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.1/css/tempusdominus-bootstrap-4.min.css" /> <!-- Tempusdominus Bbootstrap 4 -->
+  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css"> <!-- iCheck -->
+  <link rel="stylesheet" href="plugins/jqvmap/jqvmap.min.css"> <!-- JQVMap -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css"> <!-- Theme style -->
+  <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css"> <!-- overlayScrollbars -->
+  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" /> <!-- Daterange picker -->
+  <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.15/dist/summernote.min.css" rel="stylesheet"> <!-- summernote text editor-->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet"> <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://formden.com/static/cdn/bootstrap-iso.css" />
+
+</head>
+
+<style>
+    .bootstrap-iso .form-group button,
+    .bootstrap-iso form input.form-control,
+    .bootstrap-iso form textarea.form-control,
+    .bootstrap-iso form select.form-control,
+    .bootstrap-iso form .form-group-lg input.form-control,
+    .bootstrap-iso form .form-group-lg textarea.form-control,
+    .bootstrap-iso form .form-group-lg select.form-control,
+    .bootstrap-iso form .form-group-sm input.form-control,
+    .bootstrap-iso form .form-group-sm textarea.form-control,
+    .bootstrap-iso form .form-group-sm select.form-control {
+        -webkit-border-radius: 20px;
+        -moz-border-radius: 20px;
+        border-radius: 20px;
+    }
+    
+    .bootstrap-iso .formden_header h2,
+    .bootstrap-iso .formden_header p,
+    .bootstrap-iso form {
+        font-family: Arial, Helvetica, sans-serif;
+        color: black
+    }
+    
+    .bootstrap-iso form button,
+    .bootstrap-iso form button:hover {
+        color: white !important;
+    }
+    
+    .asteriskField {
+        color: red;
+    }
+    
+    .bootstrap-iso form .input-group-addon {
+
+        border-radius: 20px;
+        padding-left: 15px
+    }
+</style>
+
+ <!--       color: #d9d9d9;
+        background-color: #979797;-->
