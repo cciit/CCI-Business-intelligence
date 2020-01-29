4a34eab5820bb086f9e4217387da47da684de211
cciit <60438931+cciit@users.noreply.github.com>
2020-01-29 10:50:10 -0500

    Create header.php

---
+++ header.php
@@ -0,0 +1,34 @@
+<body class="sidebar-collapse">
+<div class="wrapper">
+<nav class="main-header navbar navbar-expand navbar-white navbar-light"> <!-- Navbar -->
+        <!-- <a href="index.php" class="brand-link"><img src="images/logo.png" alt="CCI Logo" width="200" height="50"> <!-- Brand Logo -->
+  <ul class="navbar-nav"> <!-- Left navbar links -->
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="index2.php" class="nav-link">Home</a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="create.php " class="nav-link">Create a User</a>
+      </li>
+    </ul>
+    <form class="form-inline ml-3"> <!-- SEARCH FORM -->
+      <div class="input-group input-group-sm">
+        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+        <div class="input-group-append">
+          <button class="btn btn-navbar" type="submit">
+            <i class="fas fa-search"></i>
+          </button>
+        </div>
+      </div>
+    </form>
+    <ul class="navbar-nav ml-auto"> <!-- Right navbar links -->
+      <li class="nav-item"> <!-- Messages Dropdown Menu -->
+        <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#">
+          <i class="fas fa-th-large"></i>
+        </a>
+      </li>
+    </ul>
+  </nav><!-- /.navbar -->
+
